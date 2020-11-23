ed1758f927bb2f163f5fdb21a7b685623696e2e8
fvidak <fvidak00@fesb.hr>
2020-11-23 19:33:45 +0100

    23

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,9 +6,12 @@ import { Component } from "@angular/core";
   styleUrls:["./post-list.component.css"]
 })
 export class PostListComponent{
+  // posts=[
+  //   {title:"1st Post", content:"This is 1st post's content."},
+  //   {title:"2nd Post", content:"This is 2nd post's content."},
+  //   {title:"3rd Post", content:"This is 3rd post's content."},
+  // ]
   posts=[
-    {title:"1st Post", content:"This is 1st post's content."},
-    {title:"2nd Post", content:"This is 2nd post's content."},
-    {title:"3rd Post", content:"This is 3rd post's content."},
+
   ]
 }
