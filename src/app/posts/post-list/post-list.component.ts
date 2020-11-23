10cea41bd4f9df8b9e4695eeb4ae1dc37a833593
fvidak <fvidak00@fesb.hr>
2020-11-23 19:43:26 +0100

    24

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector:"app-post-list",
@@ -11,7 +11,5 @@ export class PostListComponent{
   //   {title:"2nd Post", content:"This is 2nd post's content."},
   //   {title:"3rd Post", content:"This is 3rd post's content."},
   // ]
-  posts=[
-
-  ]
+  @Input() posts=[]
 }
