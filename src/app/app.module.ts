911f0c93df5d4ef3f5f6edba58d8ef2a395c1424
fvidak <fvidak00@fesb.hr>
2020-11-23 18:19:05 +0100

    19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms"
 
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component'
@@ -8,9 +9,11 @@ import {PostCreateComponent} from './posts/post-create/post-create.component'
   declarations: [
     AppComponent,
     PostCreateComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
