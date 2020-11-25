3b566b322f3f242114a58bff54ed0b7b2ad2ac0c
fvidak <fvidak00@fesb.hr>
2020-11-25 19:32:24 +0100

    38

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,24 @@ const express=require("express")
 
 const app=express()
 
-app.use((req,res,next)=>{
-  console.log("1st middleware")
-  next()
-})
+app.use("/api/posts",(req,res,next)=>{
+  const posts=[
+    {
+      id:"fdhg5476",
+      title:"1st server-side post",
+      content:"This is coming from the server."
+    },
+    {
+      id:"zuioqwer",
+      title:"2nd server-side post",
+      content:"This is coming from server"
+    }
+  ]
 
-app.use((req,res,next)=>{
-  res.send("Hello from express!")
+  res.status(200).json({
+    message:"Posts fetched succesfully",
+    posts:posts
+  })
 })
 
 module.exports=app
