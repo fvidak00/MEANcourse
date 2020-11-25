ed8a70e6d779cf69e2bb91d1d82ce734dc40413d
fvidak <fvidak00@fesb.hr>
2020-11-25 21:44:38 +0100

    40

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,15 @@ const express=require("express")
 
 const app=express()
 
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*")
+  res.setHeader("Access-Control-Allow-Header",
+                "Origin, X-Requested-With, Content-Type, Accept")
+  res.setHeader("Access-Comtrol-Allow-Methods",
+                  "GET, POST, PATCH, DELETE, OPTIONS")
+  next()
+})
+
 app.use("/api/posts",(req,res,next)=>{
   const posts=[
     {
