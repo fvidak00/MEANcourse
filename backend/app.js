754b8f1ea732442107b5c60e1b42c2cc668c4f48
fvidak <fvidak00@fesb.hr>
2020-11-26 01:14:43 +0100

    57

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","*")
   res.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept")
-  res.setHeader("Access-Comtrol-Allow-Methods",
+  res.setHeader("Access-Control-Allow-Methods",
                   "GET, POST, PATCH, DELETE, OPTIONS")
   next()
 })
@@ -53,6 +53,13 @@ app.get("/api/posts",(req,res,next)=>{
         posts:documents
       })
     })
+})
+
+app.delete("/api/posts/:id",(req,res,next)=>{
+  Post.deleteOne({_id:req.params.id}).then(result=>{
+    console.log(result)
+  })
+  res.status(200).json({message:"Post deleted"})
 })
 
 module.exports=app
