9f8d67d1befbfc04245beecf33a1b4d24ef8088f
fvidak <fvidak00@fesb.hr>
2020-11-27 18:22:19 +0100

    67

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,29 +15,34 @@ export class PostCreateComponent implements OnInit{
   enteredContent=""
   private mode="create"
   private postId:string;
-  private post:Post
+  post:Post
 
   constructor(public postsService:PostsService, public route:ActivatedRoute){}
 
-ngOnInit(){
-  this.route.paramMap.subscribe((paramMap:ParamMap)=>{
-    if(paramMap.has("postId")){
-      this.mode="edit"
-      this.postId=paramMap.get("postId")
-      this.post=this.postsService.getPost(this.postId)
-    }
-    else{
-      this.mode="create"
-      this.postId=null
-    }
-  })
-}
+  ngOnInit(){
+    this.route.paramMap.subscribe((paramMap:ParamMap)=>{
+      if(paramMap.has("postId")){
+        this.mode="edit"
+        this.postId=paramMap.get("postId")
+        this.post=this.postsService.getPost(this.postId)
+      }
+      else{
+        this.mode="create"
+        this.postId=null
+      }
+    })
+  }
 
-  onAddPost(form: NgForm){
+  onSavePost(form: NgForm){
     if (form.invalid){
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content)
+    if(this.mode=="create") {
+      this.postsService.addPost(form.value.title, form.value.content)
+    }
+    else{
+      this.postsService.updatePost(this.postId,form.value.title, form.value.content)
+    }
     form.resetForm()
   }
 }
