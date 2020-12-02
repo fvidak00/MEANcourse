ba573f34d019b5f0697c6ae7883edf9990de8c25
fvidak <fvidak00@fesb.hr>
2020-12-02 21:13:54 +0100

    109

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { AuthData } from './auth-data.model';
 
@@ -11,7 +12,7 @@ export class AuthService{
   private authStatusListener=new Subject<boolean>()
   private isAuthenticated=false
 
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient, private router:Router){}
 
   getToken(){
     return this.token
@@ -50,6 +51,7 @@ export class AuthService{
         if(token){
           this.isAuthenticated=true
           this.authStatusListener.next(true)
+          this.router.navigate(["/"])
         }
       })
   }
@@ -58,5 +60,6 @@ export class AuthService{
     this.token=null
     this.isAuthenticated=false
     this.authStatusListener.next(false)
+    this.router.navigate(["/"])
   }
 }
