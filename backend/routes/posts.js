c89095a9c03b7e2f1f491cff5a65cc192f576749
fvidak <fvidak00@fesb.hr>
2020-12-02 14:50:38 +0100

    81

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -28,15 +28,27 @@ const storage=multer.diskStorage({
   }
 })
 
+
 router.post("", multer({storage:storage}).single("image") ,(req,res,next)=>{
+  const url=req.protocol+"://"+req.get("host")
   const post=new Post({
     title:req.body.title,
-    content:req.body.content
+    content:req.body.content,
+    imagePath:url+"/images/"+req.file.filename
   })
   post.save().then(createdPost=>{
     res.status(201).json({
       message:"Post added successfully",
-      postId:createdPost._id
+      post:{
+        // This:
+        // ...createdPost
+        id:createdPost._id,
+        // Instead of this block
+        title:createdPost.title,
+        content:createdPost.content,
+        imagePath:createdPost.imagePath
+        //
+      }
     })
   })
 })
