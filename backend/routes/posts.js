0cde2086b0d3f838545f2623991423368b829199
fvidak <fvidak00@fesb.hr>
2020-12-03 22:49:33 +0100

    130

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,8 @@
 const express=require("express")
 const multer=require("multer")
 
-const Post=require("../models/post")
 const checkAuth=require("../middleware/check-auth")
+const PostsController=require("../controllers/posts")
 
 const router=express.Router()
 const MIME_TYPE_MAP={
@@ -31,180 +31,29 @@ const storage=multer.diskStorage({
 router.post("",
   checkAuth,
   multer({storage:storage}).single("image"),
-  (req,res,next)=>{
-    const url=req.protocol+"://"+req.get("host")
-    const post=new Post({
-      title:req.body.title,
-      content:req.body.content,
-      imagePath:url+"/images/"+req.file.filename,
-      creator:req.userData.userId
-    })
-    post.save()
-      .then(
-        createdPost=>{
-          res.status(201).json({
-            message:"Post added successfully",
-            post:{
-              // This:
-              // ...createdPost
-              id:createdPost._id,
-              // Instead of this block
-              title:createdPost.title,
-              content:createdPost.content,
-              imagePath:createdPost.imagePath
-              //
-            }
-          })
-        }
-      )
-      .catch(
-        error=>{
-          res.status(500).json({
-            message:"Post creation failed!"
-          })
-        }
-      )
-})
+  PostsController.createPost
+)
 
 
 router.put("/:id",
   checkAuth,
   multer({storage:storage}).single("image"),
-  (req,res,next)=>{
-    let imagePath=req.body.imagePath
-    if(req.file){
-      const url=req.protocol+"://"+req.get("host")
-      imagePath=url+"/images/"+req.file.filename
-    }
-    const post=new Post({
-      _id:req.body.id,
-      title:req.body.title,
-      content:req.body.content,
-      imagePath:imagePath,
-      creator:res.userData.userId
-    })
-    Post.updateOne({
-        _id:req.params.id,
-        creator:req.userData.userId
-      },
-      post
-    )
-      .then(
-        result=>{
-          if(result.nModified>0){
-            res.status(200).json({
-              message:"Update successful!"
-            })
-          }
-          else{
-            res.status(401).json({
-              message:"Not authorized!"
-            })
-          }
-        }
-      )
-      .catch(
-        error=>{
-          res.status(500).json({
-            message:"Couldn't update post!"
-          })
-        }
-      )
-})
+  PostsController.updatePost
+)
 
 
 router.get("",
-  (req,res,next)=>{
-    const pageSize=+req.query.pagesize
-    const currentPage=req.query.page
-    const postQuery=Post.find()
-    let fetchedPosts
-    if(pageSize && currentPage){
-      postQuery
-        .skip(pageSize*(currentPage-1))
-        .limit(pageSize)
-    }
-    postQuery
-      .then(
-        documents=>{
-          fetchedPosts=documents
-          return Post.countDocuments()
-        }
-      )
-      .then(
-        count=>{
-          res.status(200).json({
-            message:"Posts fetched successully!",
-            posts:fetchedPosts,
-            maxPosts:count
-          })
-        }
-      )
-      .catch(
-        error=>{
-          res.status(500).json({
-            message:"Fetching posts failed!"
-          })
-        }
-      )
-  }
+  PostsController.getAllPosts
 )
 
 router.get("/:id",
-  (req,res,next)=>{
-    Post.findById(req.params.id)
-      .then(
-        post=>{
-          if(post){
-            res.status(200).json(post)
-          }
-          else{
-            res.status(400).json({
-              message:"Post not found!"
-            })
-          }
-        }
-      )
-      .catch(
-        error=>{
-          res.status(500).json({
-            message:"Fetching post failed!"
-          })
-        }
-      )
-  }
+  PostsController.getPostById
 )
 
 
 router.delete("/:id",
   checkAuth,
-  (req,res,next)=>{
-    Post.deleteOne({
-      _id:req.params.id,
-      creator:req.userData.userId
-    })
-      .then(
-        result=>{
-          if(result.n>0){
-            res.status(200).json({
-              message:"Deletion successful!"
-            })
-          }
-          else{
-            res.status(401).json({
-              message:"Not authorized"
-            })
-          }
-        }
-      )
-      .catch(
-        error=>{
-          res.status(500).json({
-            message:"Fteching posts failed!"
-          })
-        }
-      )
-  }
+  PostsController.deletePost
 )
 
 
